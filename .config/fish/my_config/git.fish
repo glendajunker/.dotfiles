b47327a7469f8f906129ed030f75db7453dce67c
glendajunker <glenda.junker@audibene.de>
2024-02-27 19:15:17 +0100

    [fish] Add git aliases

---
+++ .config/fish/my_config/git.fish
@@ -0,0 +1,2 @@
+abbr --add gg 'git log --oneline --decorate --color --graph --all'
+abbr --add gga 'git log --graph --decorate --format=format:"%C(yellow)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)%cr %C(bold blue)<%an>%C(reset)" --all'
--- .config/fish/my_config/index.fish
+++ .config/fish/my_config/index.fish
@@ -2,3 +2,4 @@ source $HOME/.config/fish/my_config/aliases.fish
 source $HOME/.config/fish/my_config/brew.fish
 source $HOME/.config/fish/my_config/dotfiles.fish
 source $HOME/.config/fish/my_config/fzf.fish
+source $HOME/.config/fish/my_config/git.fish
