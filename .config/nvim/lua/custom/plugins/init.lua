d0e9abbb8337b4d25f95b00e0a22fb71f22f1c11
glendajunker <glenda.junker@audibene.de>
2024-02-29 19:28:09 +0100

    [nvim] Install oil plugin & update config

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      default_file_explorer = true,
+      delete_to_trash = true,
+    },
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+}
