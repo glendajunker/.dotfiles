db6b2714ba62b03948f3faecabc03dc17dfb2828
glendajunker <glenda.junker@audibene.de>
2024-02-29 18:32:12 +0100

    [fish] Add nvim alias

--- .config/fish/my_config/index.fish
+++ .config/fish/my_config/index.fish
@@ -3,3 +3,4 @@ source $HOME/.config/fish/my_config/brew.fish
 source $HOME/.config/fish/my_config/dotfiles.fish
 source $HOME/.config/fish/my_config/fzf.fish
 source $HOME/.config/fish/my_config/git.fish
+source $HOME/.config/fish/my_config/nvim.fish
